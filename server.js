1b28e26372d9e89e3517ecef9f8c8009b73fe863
Junah Park <junahp@hotmail.com>
2021-02-06 22:00:57 -0800

    save server.js

--- server.js
+++ server.js
@@ -5,15 +5,13 @@ const app = express();
 const port = process.env.PORT || 8080;
 // const path = require('path');
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname, 'build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-}
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 // Our DB Configuration
 // require('./src/database');
